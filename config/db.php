d86f4181c870125080c3b7958969f37a6fb22abd
merkis88 <axuenno88@gmail.com>
2025-07-04 16:26:54 +0700

    db coection

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+use illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'internship',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
